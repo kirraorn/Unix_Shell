b2bba60f595cdace20470741ec7f58a8fd57859b
LUDGINIE  DORVAL <80278412+Ludginie@users.noreply.github.com>
2025-09-16 17:14:34 -0400

    Update shellprompt.c
    Updated shellprompt.c to match main

--- starter/src/shellprompt.c
+++ starter/src/shellprompt.c
@@ -2,18 +2,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h> //sets limits for the host and path arrays
+#include <string.h>
 
 int main() {
 
 char hostname[HOST_NAME_MAX]; //make arrays for the host and path names
 char cwd[PATH_MAX];
+char input[MAX_INPUT];
 
-char *user = getenv("USER"); //get username, cwd, and host name
-getcwd(cwd, sizeof(cwd));
-gethostname(hostname, sizeof(hostname));
+while (1) {
+  char *user = getenv("USER"); //get username, cwd, and host name
+  getcwd(cwd, sizeof(cwd));
+  gethostname(hostname, sizeof(hostname));
 
-printf("%s@%s:%s> ", user, hostname, cwd); //print them out with formatting
+  printf("%s@%s:%s> ", user, hostname, cwd); //print them out with formatting
 
-return 0;
+  if (fgets(input, sizeof(input), stdin) == NULL) // get user input/ if using control D then just go to new line and exit
+  {
+      printf("\n");
+      break;
+  }
+
+  input[strcspn(input, "\n")] = 0; // get rid of newline for future parsing
+
+  if (strcmp(input, "exit") == 0) // if user actually types exit, then exit
+  { break; }
+}
+  return 0;
 
 }
